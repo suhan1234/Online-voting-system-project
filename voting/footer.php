8d60e82b76a0175744f8f60f7d3d9bb3fd1d1de3
Suhan B T <suhanbt@gmail.com>
2024-08-21 23:06:04 +0530

    j

---
+++ voting/footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2024. Developed by SUHAN : BIT1510297.
+<br/>
+Online Voting System.
+<br/>
+Final Year Project 2022. University Of abc
+<br/>
+Contact: <a href="tel:+260979848064">+260 979-848064</a> | Email: <a href="mailto:   ">abc@gmail.com</a>
+
+</footer>
---
+++ voting/register/footer.php
@@ -0,0 +1,12 @@
+
+<div class="footer">
+	<hr/>
+&copy2022. Developed by SUHAN : BIT1510297.
+<br/>
+Online Voting System.
+<br/>
+Final Year Project 2022. University Of ABC
+<br/>
+Contact: <a href="tel:+260979848064">+260 979-848064</a> | Email: <a href="mailto:  ">abc@gmail.com</a>
+
+</div>
